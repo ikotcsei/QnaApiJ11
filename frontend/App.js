f6e3f9fb68b111b29cb45b79ed901cc0a852ac75
ikotcsei <ikotcsei@gmail.com>
2024-01-15 15:21:41 +0100

    converting object to json, implicit conversion not working

--- frontend/App.js
+++ frontend/App.js
@@ -21,27 +21,44 @@ class App extends Component{
             console.log(persons);
             this.setState({ persons });
         })
+        .catch((error) => {
+              console.log(error.message)
+        })
 
         console.log(Object.keys(this.state.persons));
     }
 
    render(){
-      console.log("links lista  type: " + typeof this.state.persons.links);
-      console.log(Object.keys(this.state.persons));
-      console.log("links lista : " + this.state.persons.links);
+
+
+//      var alist;
+//
+//      if( isEmpty(alist) ){
+//                    console.log("links list is empty.");
+//                    if(alist === undefined){
+//                        console.log("list is undefined.");
+//                    }
+//                  } else{
+//                    console.log("links list is not empty");
+//      }
+
+
+
+
+//      console.log("links lista  type: " + typeof this.state.persons.links);
+//      console.log(Object.keys(this.state.persons));
+//      console.log("links lista : " + this.state.persons.links);
       const links = this.state.persons.links;
       const reptiles = ['alligator', 'snake', 'lizard'];
 
-     {/* if (typeof links === undefined ) {
-         console.log("undefined : ");
-      }else {
-         console.log("defined" + links);
-      }*/}
+//        objectMethods(this.state.persons);
+
 
       return(
          <div>
             <h1>{this.state.persons.question}</h1>
             <div className="content" dangerouslySetInnerHTML={{__html: this.state.persons.htmlAnswer}}></div>
+            <pre>{JSON.stringify(this.state.persons, null, 2)}</pre>
 
          </div>
 
@@ -52,4 +69,74 @@ class App extends Component{
 
 
 }
+
+
+
+function isEmpty( list ){
+    if( list && list.length ){
+            return false;
+    }
+    return true;
+}
+
+function jsonIsEmpty( jsonObj ){
+    return isEmpty(Object.entries( jsonObj ));
+}
+
+function objectIsUndefined( object ){
+    if( object === undefined){
+        return 1;
+    }
+    return 0;
+}
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+function objectMethods( list ){
+    console.log("Object.keys");
+    console.log(Object.keys(list));
+    console.log("Object.values");
+    console.log(Object.values(list));
+
+
+
+}
+
+function runTests(){
+
+    const object1 = {
+        a: 'somestring',
+        b: 42,
+    };
+
+    var object2 ={};
+
+    if( jsonIsEmpty(object1) ){
+        console.log("jsonObj is empty.");
+    } else{
+        console.log("jsonObj is not empty");
+    }
+
+    if( jsonIsEmpty(object2) ){
+            console.log("jsonObj is empty.");
+          } else{
+            console.log("jsonObj is not empty");
+    }
+
+    var obji1;
+            var obji2 = 2;
+
+            if(objectIsUndefined(obji1)){
+                console.log("undefined object .");
+            } else{
+              console.log("object is definded.")
+            }
+
+            if(objectIsUndefined(obji2)){
+                        console.log("undefined object .");
+            }else{
+                       console.log("object is definded.")
+            }
+}
+
+
 export default App;
