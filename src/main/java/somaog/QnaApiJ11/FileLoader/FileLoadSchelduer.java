785ca8a8b46d0437a73f5e858b78b5b874105958
ikotcsei <ikotcsei@gmail.com>
2026-07-11 10:20:31 +0200

    FE images jobb, restyle

---
+++ src/main/java/somaog/QnaApiJ11/FileLoader/FileLoadSchelduer.java
@@ -0,0 +1,11 @@
+package somaog.QnaApiJ11.FileLoader;
+
+/*
+   ez a class ca 60 masodpercenket ellenorzi h volt e a fileokban modositas, es ha igen
+   reloadolja oket a memoriaba. a memoriat szinkronizalni kell az iras idejere.
+   - read all files in root dir, print msg
+
+*
+*/
+public class FileLoadSchelduer {
+}
