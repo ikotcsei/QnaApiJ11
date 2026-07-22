4a7a8dd6a811f664bb1c75012bc319fa65fba9e4
ikotcsei <ikotcsei@gmail.com>
2026-07-22 22:42:56 +0200

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	src/main/java/somaog/QnaApiJ11/controller/HtmlController.java
    #	src/main/java/somaog/QnaApiJ11/repository/DesignPatternRepositoryInOrder.java
    #	src/main/java/somaog/QnaApiJ11/repository/InorderTetelRepository.java
    #	src/main/java/somaog/QnaApiJ11/repository/JavaBasicRepositoryInOrder.java
    #	src/main/java/somaog/QnaApiJ11/repository/RandomTetelRepository.java
    #	src/main/java/somaog/QnaApiJ11/service/TetelService.java

--- src/main/java/somaog/QnaApiJ11/repository/InOrderTetelRepository.java
+++ src/main/java/somaog/QnaApiJ11/repository/InOrderTetelRepository.java
@@ -1,36 +1,18 @@
+
 package somaog.QnaApiJ11.repository;
-
-import somaog.QnaApiJ11.XMLInputHandlers.Tetel;
-import somaog.QnaApiJ11.XMLInputHandlers.TetelListManager;
-
-import java.util.Iterator;
 
 /*
     class for managing tetels inorder - normal - mode
 * */
 public class InOrderTetelRepository extends TetelRepository{
 
-//    private Iterator<Tetel> it = null;
 
-    InOrderTetelRepository(){
-        tetelListManager = TetelListManager.createNewEmpty();
-//        it = tetelListManager.getTetelList().iterator();
-        System.out.println("whatever");
-    }
     public void initNextTetel(){
 
     }
 
-//    public Tetel readNext(){
-//        if(!it.hasNext()){
-//            //reset iterator to item 0, than return item 0
-//            it = tetelListManager.getTetelList().iterator();
-//            return it.next();
-//        }
-//        return it.next();
-//    }
-
     @Override
     void initTetelList() throws Exception {
+        //create iterator, set iterator to first  item
     }
 }
